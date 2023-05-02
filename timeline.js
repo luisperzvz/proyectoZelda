ae3a0c299168e4dba6854f291575e6ec3f118c11
HACKER <nescaruncho@gmail.com>
2023-05-03 01:33:43 +0200

    js arreglado con evento submit formulario

--- timeline.js
+++ timeline.js
@@ -3,13 +3,15 @@
 const results = await fetch('./zelda-timeline.json');
 const zeldaTimeline = await results.json();
 
+const formElement = document.forms.addGame;
+
 const ulElement = document.getElementById('zeldaGames')
 
 async function getTimeline() {
 	try {
-		let response = await zeldaTimeline;
-		//Ordenación de los juegos 
-		response = response.sort((a, b) => parseFloat(a.date) - parseFloat(b.date));
+		let response = localStorage.getItem('timeline') ? JSON.parse(localStorage.getItem('timeline')) : await zeldaTimeline;
+        
+        localStorage.setItem('timeline', JSON.stringify(response));
 
 		return response;
 	} catch (error) {
@@ -20,15 +22,18 @@ async function getTimeline() {
 }
 
 async function createUlContent () {
+    ulElement.innerHTML = '';
     let fragment = document.createDocumentFragment();
     let results = await getTimeline();
+
+    results = results.sort((a, b) => parseFloat(a.date) - parseFloat(b.date));
+
     for(let game of results){
-		console.log(game);
         let li = document.createElement('li');
         li.innerHTML = `
             <div class="game">
                 <h2>${game.date}</h2>
-                <h3>${game.title}</h3>
+                <p>${game.title}</p>
                 <img src="${game.image}" alt="Image ${game.title}" />
                 <p>${game.text}</p>
             </div>
@@ -41,3 +46,37 @@ async function createUlContent () {
 }
 
 createUlContent();
+
+async function handleSubmit(event) {
+    event.preventDefault();
+
+    let formData = new FormData(formElement);
+    let title = formData.get('title');
+    let date = parseInt(formData.get('date'));
+    let text = formData.get('text');
+    let image = formData.get('image');
+
+    console.log(title, date, text, image);
+
+    let newGame = {
+        title,
+        date,
+        text,
+        image
+    }
+
+    console.log(newGame);
+
+    let results = await getTimeline();
+
+    results.push(newGame);
+
+    localStorage.setItem('timeline', JSON.stringify(results));
+
+    formElement.reset();
+
+    createUlContent();
+
+}
+
+formElement.addEventListener('submit', handleSubmit);
